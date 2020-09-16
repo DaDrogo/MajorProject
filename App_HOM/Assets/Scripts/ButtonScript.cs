9d63593047b4212785a99eaea8a23d07b9516f1f
DaDrogo <38428733+DaDrogo@users.noreply.github.com>
2020-09-16 18:15:37 +0200

    LogInScene

--- App_HOM/Assets/Scripts/ButtonScript.cs
+++ App_HOM/Assets/Scripts/ButtonScript.cs
@@ -1,23 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
     [SerializeField]
-    private GameObject Button;
+    private GameObject UserTxt;
+    [SerializeField]
+    private GameObject PassTxt;
+
+    [SerializeField]
+    private GameObject WarningTxt;
+
+    int correct;
+
+    private void Start()
+    {
+        UserTxt.GetComponent<Text>();
+        PassTxt.GetComponent<Text>();
+        correct = 0;
+    }
+
 
     public void LogIn()
     {
+        //sende Strings zum einloggen an die Datenbank
+        //erhalte ein Signal ob korrekt oder fehlerhaft
+        //bei erfolg wechsel des Fensters mit den daten der bank bei misserfolg Nachricht durch Text
+        if (correct == 0)
+        {
 
+        }
+        else
+        {
+
+        }
     }
 
-    public void SingIn()
+    public void SingUp()
     {
+        //sende Strings zum speichern an die Datenbank
+        //erhalte ein Signal ob vorhanden oder frei
+        //bei erfolg wechsel des Fensters und speicherung der daten bei misserfolg Nachricht durch Text
 
+        if (correct == 0)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 
-    public void LogIn()
+    public void LogOut()
     {
 
     }
