44375b423122dce37ac14e1462e57b16622a5ae6
DaDrogo <38428733+DaDrogo@users.noreply.github.com>
2020-09-24 18:41:30 +0200

    Txt Dateien
    Txt Dateien wurden eingefügt und bearbeitet

--- App_HOM/Assets/Scripts/CreationScript.cs
+++ App_HOM/Assets/Scripts/CreationScript.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CreationScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private TMP_Text DescriptionText;
+
+ 
+
+    public void DescribeDropdown(int Drop)
     {
-        
+
+            DescriptionText.text = ParseFile("Völker", Drop);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    //liest eine Linie einer TXT Datei und gibt diese wieder
+    string ParseFile(string File, int Nr)
     {
-        
+        //muss noch überarbeitet werden suche des Speicherorts
+        //TXT Dateien aus dem Server holen
+        string[] Text = System.IO.File.ReadAllLines("D:\\Programme\\GitHub\\MajorProject\\App_HOM\\Assets\\DescriptionTexts\\" + File+ ".txt");
+        return (Text[Nr]);
     }
 }
