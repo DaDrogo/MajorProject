4968f4cbe3a698f783018e1a9183b8c6779611ac
DaDrogo <38428733+DaDrogo@users.noreply.github.com>
2020-09-24 15:11:31 +0200

    Serververbindung hergestellt
    Mit Konsti gearbeitet. Push ein Tag zu spät.

---
+++ PHP_Scripts/Register.php
@@ -0,0 +1,34 @@
+<?php
+$hostname = "localhost";
+$database = "examplename";
+$database_username = "root";
+$database_password = "";
+
+$connection = mysqli_connect($hostname, $database_username, $database_password, $database);
+if(mysqli_connect_errno())
+{
+	die("halt stop");
+}
+else
+{
+	$username = $_POST['username'];
+	
+	$passport = $_POST['passport'];
+	
+	//$query = "INSERT INTO 'login' ( Username, Passport) VALUES ($)";
+	$query = "INSERT INTO login ( Username, Passport) VALUES ('$username', '$passport')";
+	$result = mysqli_query($connection, $query);
+	if($result)
+	{
+		
+		$querys = "SELECT Passport FROM login WHERE Username=$username";
+		$results = mysqli_query($connection, $querys);
+		echo $results;
+		echo "ha";
+	}
+	else
+	{
+		echo "lol";
+	}
+}
+?>
