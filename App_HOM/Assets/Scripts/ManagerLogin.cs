4848ead3711b86985717ffa125637944f60a3aca
DaDrogo <38428733+DaDrogo@users.noreply.github.com>
2020-10-12 02:09:37 +0200

    Login/Menu Unity fertig

--- App_HOM/Assets/Scripts/ManagerLogin.cs
+++ App_HOM/Assets/Scripts/ManagerLogin.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ManagerLogin : MonoBehaviour
 {
     public GameObject ID;
+
+
+    public TMP_Text Username;
+    public TMP_Text Passport;
+    public TMP_Text Warning;
 
     private void Awake()
     {
@@ -13,6 +21,78 @@ public class ManagerLogin : MonoBehaviour
             Object.Destroy(Gonk);
 
         Instantiate(ID);
+    }
+
+    private void Start()
+    {
+        Username.text = "";
+        Passport.text = "";
+        Warning.text = ""; 
+    }
+
+    public void PressButton(string url)
+    {
+        //sende Strings zum einloggen an die Datenbank
+        //erhalte ein Signal ob korrekt oder fehlerhaft
+        //bei erfolg wechsel des Fensters mit den daten der bank bei misserfolg Nachricht durch Text
+
+        int userLE = Username.text.Length;
+        int passLE = Passport.text.Length;
+
+        if (userLE >=2 && passLE>=2  )
+        {
+            
+            Debug.Log(Username.text);
+            Debug.Log(Passport.text);
+            Warning.text = "Danke";
+            //StartCoroutine(ConnectDatabase(url));
+            GameObject.FindGameObjectWithTag("ID").GetComponent<PlayerID>().ID = 1;
+            ToMenu();
+        }
+        else if(userLE <= 1)
+        {
+            Warning.text = "Bitte Benutzernamen eingeben.";
+        }
+        else if (passLE <= 1)
+        {
+            Warning.text = "Bitte Passwort eingeben.";
+        }
+        else
+        {
+            Warning.text = "Bitte etwas eingeben";
+        }
+    }
+
+    public void LogOut()
+    {
+
+    }
+
+    public IEnumerator ConnectDatabase(string url)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", Username.text);
+        form.AddField("passport", Passport.text);
+        UnityWebRequest request = UnityWebRequest.Post(url, form);
+        yield return request.Send();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.LogError("Networkerror");
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+            //Test soll die ID von PHP bekommen
+            // wird noch getestet und wo es hingehört
+            GameObject.FindGameObjectWithTag("ID").GetComponent<PlayerID>().ID = 1;
+            request.Dispose();
+        }
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene(1);
     }
 
 }
---
+++ App_HOM/Assets/Scripts/ManagerMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ManagerMainMenu : MonoBehaviour
+{
+
+    //was will ich haben?
+    //1. abfrage nach id ob es schon Bögen gibt, anzahl an bögen wird in array gespeichert und buttons mit deren name erstellt. also 2 arrays
+    //Außerdem wird ein Button für zurück benötigt
+    //Main Menu beinhaltet ersmal nur buttons. Diese führen zu
+    //1. login zurück
+    //2. Erstellen von Fragebogen
+    //3. Auswahl der erstellten Fragebögen
+    //ez Arbeit kommt bei den Fragebögen
+
+    public void SwitchScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
